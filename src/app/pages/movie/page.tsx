c588b13551520f646fd7062dd7b6153743750bc2
Jimmy Joice <jimjoice28@gmail.com>
2024-01-23 12:28:18 +0530

    movie section prettifying done

--- src/app/pages/movie/page.tsx
+++ src/app/pages/movie/page.tsx
@@ -8,28 +8,32 @@ const Page = () => {
   const data = useRecoilValue(dataShare);
   console.log("data => ", data);
   return (
-    <div className="h-screen w-screen  bg-[#C5FAD5]">
+    <div className="h-auto w-screen bg-[#C5FAD5] flex flex-wrap justify-center items-center">
       {data.Response === "True" ? (
-        <div className="">
-          {data.Search.map((item, index) => {
-            return (
-              <div key={index}>
-                <h1>{item.Title}</h1>
-                <h1>{item.Year}</h1>
-                <h1>{item.imdbID}</h1>
-                <h1>{item.Type}</h1>
-                <Image
-                  src={item.Poster}
-                  alt="poster"
-                  width={100}
-                  height={100}
-                />
+        data.Search.map((item, index) => {
+          return (
+            <div
+              className="m-4 bg-white shadow-lg rounded-lg overflow-hidden"
+              key={index}
+            >
+              <Image
+                src={item.Poster}
+                alt="poster"
+                width={200}
+                height={200}
+                className="w-full h-64 object-cover object-center"
+              />
+              <div className="p-6 font-bold">
+                <h1 className="text-xl font-bold"> Title {item.Title}</h1>
+                <h2 className="text-gray-700"> Year {item.Year}</h2>
+                <h3 className="text-gray-500"> Type {item.Type}</h3>
+                <h4 className="text-sm text-gray-500"> imdbID {item.imdbID}</h4>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })
       ) : (
-        <div>
+        <div className="text-xl font-bold h-screen w-screen flex justify-center items-center">
           <h1>No Data To Display Here</h1>
         </div>
       )}
